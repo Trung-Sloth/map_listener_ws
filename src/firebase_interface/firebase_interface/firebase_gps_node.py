85170902d269eba92660a713c48369bcccc00dcf
Trung-Sloth <tranquoctrung6a8@gmail.com>
2025-05-11 07:59:08 +0700

    Update firebase_gps_node.py

--- src/firebase_interface/firebase_interface/firebase_gps_node.py
+++ src/firebase_interface/firebase_interface/firebase_gps_node.py
@@ -1,10 +1,10 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Point
+from std_msgs.msg import Float32
 
 import firebase_admin
 from firebase_admin import credentials, db
-
+import numpy as np
 
 class FirebaseGPSPublisher(Node):
     def __init__(self):
@@ -15,7 +15,8 @@ class FirebaseGPSPublisher(Node):
             'databaseURL': 'https://map-1-b0eae-default-rtdb.asia-southeast1.firebasedatabase.app/'
         })
 
-        self.publisher_ = self.create_publisher(Point, 'gps_data', 10)
+        self.lat_publisher = self.create_publisher(Float32, 'lat', 10)
+        self.lng_publisher = self.create_publisher(Float32, 'lng', 10)
         self.timer = self.create_timer(2.0, self.publish_data)
 
         self.get_logger().info('Firebase GPS publisher node started.')
@@ -26,12 +27,18 @@ class FirebaseGPSPublisher(Node):
             data = ref.get()
 
             if data and 'lat' in data and 'lng' in data:
-                point_msg = Point()
-                point_msg.x = float(data['lat'])
-                point_msg.y = float(data['lng'])
+                lat = np.float(data['lat'])
+                lng = np.float(data['lng'])
 
-                self.publisher_.publish(point_msg)
-                self.get_logger().info(f'GPS: lat={point_msg.x}, lng={point_msg.y}')
+                lat_msg = Float32()
+                lng_msg = Float32()
+
+                lat_msg.data = lat
+                lng_msg.data = lng
+
+                self.lat_publisher.publish(lat_msg)
+                self.lng_publisher.publish(lng_msg)
+                self.get_logger().info(f'GPS: lat={lat}, lng={lng}')
             else:
                 self.get_logger().warn('Data not found or missing lat/lng')
 
